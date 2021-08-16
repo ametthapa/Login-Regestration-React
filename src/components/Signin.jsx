dbe619ea3427f7018bb45adfc633fc9a843a9562
Amrit Thapa <thapa.amet27@gmail.com>
2021-08-16 10:07:49 +0545

    sigin more contrast

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -5,7 +5,7 @@ import { RiLockPasswordFill } from "react-icons/ri";
 
 const Signin = () => {
   return (
-    <div className="p-6 bg-gray-100">
+    <div className="p-6 bg-gray-200">
       <div className="grid grid-cols-2 w-3/4 m-auto bg-white rounded-lg shadow-lg p-6">
         <div className="">
           <img src="signin.jpg" alt="Sign In" />
